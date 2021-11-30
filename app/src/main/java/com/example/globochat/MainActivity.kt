02f92ed4263b466226b46a415d8d1f9ca20ab139
chidubem-max <onyeukwuchidubemclinton@gmail.comu>
2021-11-30 07:39:21 +0100

    Preference setting task

--- app/src/main/java/com/example/globochat/MainActivity.kt
+++ app/src/main/java/com/example/globochat/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.globochat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.preference.PreferenceManager
 import com.example.globochat.R
 
 
@@ -28,6 +30,11 @@ class MainActivity : AppCompatActivity() {
 
        // Link ActionBar with NavController
         setupActionBarWithNavController(navController, appBarConfiguration)
+
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+
+        val autoReplyTime = sharedPreferences.getString("key_auto_reply_time", "")
+        Log.i("MainActivity","Auto Reply Time: $autoReplyTime")
     }
 
     override fun onSupportNavigateUp(): Boolean {
--- app/src/main/java/com/example/globochat/SettingsFragment.kt
+++ app/src/main/java/com/example/globochat/SettingsFragment.kt
@@ -2,18 +2,42 @@ package com.example.globochat
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.NavHostFragment
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings, rootKey)
+
+       val accSettingsPref = findPreference<Preference>(getString(R.string.key_account_settings))
+
+
+        accSettingsPref?.setOnPreferenceClickListener {
+            // do something...
+
+            val navHostFragment = activity?.supportFragmentManager?.findFragmentById(R.id.nav_host_frag) as NavHostFragment
+            val navController = navHostFragment.navController
+            val action = SettingsFragmentDirections.actionSettingsToAccSettings()
+            navController.navigate(action)
+
+            true
+        }
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
+
+        val autoReplyTime = sharedPreferences.getString("key_auto_reply_time", "")
+        Log.i("SettingFragment","Auto Reply Time: $autoReplyTime")
+
+        val autoDownload = sharedPreferences.getBoolean("key_auto_download", false)
+        Log.i("SettingFragment","Auto Download: $autoDownload")
 
     }
 }
